561d5b805b6fb83172d47e849108e5c5dd3e6306
Aminur Muda <aminurmuda@gmail.com>
2022-09-11 14:10:14 +0700

    fix: broken tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,7 +33,6 @@ function MyApp({ Component, pageProps }: AppProps) {
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
-        <script>
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
